afea0d19d657555d3039e60f7bd48789933a8ca0
Lucas Renan <lucasrenanmiranda838@gmail.com>
2026-09-06 16:27:29 -0300

    security: adiciona protecao de rotas e CSRF

--- extensions.py
+++ extensions.py
@@ -1,12 +1,15 @@
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 from flask_session import Session
+from flask_wtf.csrf import CSRFProtect
 
 from MySQLdb.cursors import DictCursor
 
 mysql = MySQL()
 bcrypt = Bcrypt()
 sess = Session()
+
+csrf = CSRFProtect()
 
 def get_cursor():
     return mysql.connection.cursor(DictCursor)
